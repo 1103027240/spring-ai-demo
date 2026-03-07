bb18e3dbfff4cdbefc48f19d6304611ee58343e7
15711961300 <yongzhong.xu@getech.cn>
2026-03-07 12:49:12 +0800

    feat:修改代码结构14

--- base-demo/src/main/java/cn/getech/base/demo/build/TextSplitterBuild.java
+++ base-demo/src/main/java/cn/getech/base/demo/build/TextSplitterBuild.java
@@ -12,7 +12,6 @@ import org.springframework.ai.transformer.splitter.TextSplitter;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
 import java.util.*;
 
 /**
