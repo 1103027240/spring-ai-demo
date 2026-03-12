76721579c1f58ad57715b29d12d8c15906ba3a6c
15711961300 <yongzhong.xu@getech.cn>
2026-03-12 18:48:21 +0800

    提交

--- base-demo/src/main/java/cn/getech/base/demo/constant/FieldValueConstant.java
+++ base-demo/src/main/java/cn/getech/base/demo/constant/FieldValueConstant.java
@@ -7,8 +7,6 @@ import java.util.regex.Pattern;
  */
 public class FieldValueConstant {
 
-    public static final String DEEPSEEK_API_KEY = "sk-b8a6686cd78144ab8d2983937429a864";
-
     // 订单号正则表达式
     public static final Pattern ORDER_NUMBER_PATTERN = Pattern.compile("([A-Z]{2,6}\\d{6,12})|(\\d{6,12})");
 
