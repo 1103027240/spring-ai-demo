b13d927bd85196f82ffa7b2137bfe1fd945a6977
15711961300 <yongzhong.xu@getech.cn>
2026-04-01 18:14:32 +0800

    增加依赖

---
+++ agent-demo/src/main/java/cn/example/agent/demo/AgentDemoApplication.java
@@ -0,0 +1,38 @@
+package cn.example.agent.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 启动类
+ * @author 11030
+ */
+@Slf4j
+@SpringBootApplication
+@EnableConfigurationProperties
+@MapperScan("cn.example.agent.demo.mapper")
+public class AgentDemoApplication {
+
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(AgentDemoApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/doc.html\n\t" +
+                "Doc: \t\thttp://" + ip + ":" + port + path + "/doc.html\n\t" +
+                "----------------------------------------------------------");
+    }
+
+}
